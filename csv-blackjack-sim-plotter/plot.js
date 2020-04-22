e78c6b17155eabd8814d3c55836c36d442b8efda
amunishkin <munishkinalex@gmail.com>
2020-04-21 19:45:34 -0700

    working multi-histogram plotter

--- csv-blackjack-sim-plotter/plot.js
+++ csv-blackjack-sim-plotter/plot.js
@@ -1,36 +1,44 @@
-/* --- Function defns --- */
+/////////////////////////////////////////////////////////////////////
+// --- Function defns --- 
 // Histogram ploting ------------------------------------------------
-function makeHistogram() 
+//  read and process data
+function readCSVHistogramData( iplayer, istrategy ) 
 {
-    // Plotting CSV data from AJAX call
-    Plotly.d3.csv("../../Blackjack-Simulator/data_out_stat/player0_0.csv", 
+    // read from specified player-strategy pair
+    var csv_file = "../../Blackjack-Simulator/data_out_stat/player";
+    csv_file += ("" + iplayer + "_" + istrategy + ".csv" );
+    // AJAX call for CSV data process + plot
+    Plotly.d3.csv(csv_file, 
         function(data){ processHistogramData(data) } 
         );
 }
-function processHistogramData( allRows ) 
+function processHistogramData( all_rows ) 
 {
-    console.log(allRows);
     var x = [];
-    for (var i=0; i<allRows.length; i++) {
-        row = allRows[i];
+    for (var i=0; i<all_rows.length; i++) {
+        row = all_rows[i];
         x.push( row['profit'] );
     }
-    console.log( 'X',x );
-    makeHistogramPlotly( x );
+    console.log( 'process histogram X',x );
+    generateHistogramPlotly(x);
 }
-function makeHistogramPlotly( x ){
+//  create data for plotting / plot when all data processed
+var traces = [];
+function generateHistogramPlotly( x )
+{
+    var trace = {
+        type: 'histogram',
+        x: x,
+        opacity: 0.5,
+    };
+    traces.push(trace);
+    if ( traces.length == 12 ) { makeHistogramPlotly(); }
+}
+function makeHistogramPlotly()
+{
     var plotDiv = document.getElementById("hist-plot-area");
-    var traces = [{
-      x: x,
-      type: 'histogram',
-      opacity: 0.5,
-      xbins: {
-        end: 4,
-        size: 1,
-        start: -4
-      }
-    }];
-    Plotly.newPlot(plotDiv, traces);
+    var layout = {barmode: "overlay"};
+    Plotly.newPlot(plotDiv, traces, layout);
 }
 
 // Pie chart ploting ------------------------------------------------
@@ -43,19 +51,18 @@ function makePieChart()
 }
 function processPieData( allRows ) 
 {
-    console.log(allRows);
+    //console.log(allRows);
     var val = 0;
     var values = [0,0,0,0,0]; // [2,inft),[1,2),[0,1),[-1,0),(-infy,-1]
     for (var i=0; i<allRows.length; i++) {
         row = allRows[i];
         val = row['profit'];
+        // sort into bins...
         if      (val >  1) { values[0]++ }
         else if (val >  0) { values[1]++ }
         else if (val == 0) { values[2]++ }
         else if (val >=-1) { values[3]++ }
         else               { values[4]++ }
-        // sort into bins...
-        console.log( 'VAL',val );
     }
     makePiePlotly( values );
 }
@@ -76,10 +83,13 @@ function makePiePlotly( values ){
     Plotly.newPlot(plotDiv, data, layout);
 }
 
-//-------------------------------------------------------------------
-/* --- MAIN CODE HERE --- */
-makeHistogram();
-makePieChart();
+/////////////////////////////////////////////////////////////////////
+// --- MAIN CODE HERE --- 
+//  plot 12 strategies for player0
+histogram_data = [];
+for (var i=0; i<12; i++) {
+    readCSVHistogramData(0,i); // new dataset
+}
 
-// Current Plotly.js version
-console.log( Plotly.BUILD );
+
+makePieChart();
