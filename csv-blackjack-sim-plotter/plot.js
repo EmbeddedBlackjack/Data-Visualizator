af807b60429953a301c9c159b4beefa97db6d3af
amunishkin <munishkinalex@gmail.com>
2020-04-21 22:48:49 -0700

    converted pie to multi-box plot

--- csv-blackjack-sim-plotter/plot.js
+++ csv-blackjack-sim-plotter/plot.js
@@ -19,11 +19,10 @@ function processHistogramData( all_rows )
         row = all_rows[i];
         x.push( row['profit'] );
     }
-    console.log( 'process histogram X',x );
     generateHistogramPlotly(x);
 }
 //  create data for plotting / plot when all data processed
-var traces = [];
+var traces_his = [];
 function generateHistogramPlotly( x )
 {
     var trace = {
@@ -31,65 +30,65 @@ function generateHistogramPlotly( x )
         x: x,
         opacity: 0.5,
     };
-    traces.push(trace);
-    if ( traces.length == 12 ) { makeHistogramPlotly(); }
+    traces_his.push(trace);
+    if ( traces_his.length == 12 ) { makeHistogramPlotly(); }
 }
 function makeHistogramPlotly()
 {
     var plotDiv = document.getElementById("hist-plot-area");
     var layout = {barmode: "overlay"};
-    Plotly.newPlot(plotDiv, traces, layout);
+    Plotly.newPlot(plotDiv, traces_his, layout);
 }
 
-// Pie chart ploting ------------------------------------------------
-function makePieChart() 
+// Box ploting ------------------------------------------------------
+//  read and process data
+function readCSVBoxData( iplayer, istrategy ) 
 {
-    // Plotting CSV data from AJAX call
-    Plotly.d3.csv("../../Blackjack-Simulator/data_out_stat/player0_0.csv", 
-        function(data){ processPieData(data) } 
+    // read from specified player-strategy pair
+    var csv_file = "../../Blackjack-Simulator/data_out_stat/player";
+    csv_file += ("" + iplayer + "_" + istrategy + ".csv" );
+    // AJAX call for CSV data process + plot
+    Plotly.d3.csv(csv_file, 
+        function(data){ processBoxData(data) } 
         );
 }
-function processPieData( allRows ) 
+function processBoxData( all_rows ) 
 {
-    //console.log(allRows);
-    var val = 0;
-    var values = [0,0,0,0,0]; // [2,inft),[1,2),[0,1),[-1,0),(-infy,-1]
-    for (var i=0; i<allRows.length; i++) {
-        row = allRows[i];
-        val = row['profit'];
-        // sort into bins...
-        if      (val >  1) { values[0]++ }
-        else if (val >  0) { values[1]++ }
-        else if (val == 0) { values[2]++ }
-        else if (val >=-1) { values[3]++ }
-        else               { values[4]++ }
+    var x = [];
+    for (var i=0; i<all_rows.length; i++) {
+        row = all_rows[i];
+        x.push( row['profit'] );
     }
-    makePiePlotly( values );
+    generateBoxPlotly(x);
 }
-function makePiePlotly( values ){
-    var plotDiv = document.getElementById("pie-plot-area");
-    var data = [{
-        type: 'pie',
-        values: values,
-        labels: ["(1,inft)","(0,1]","0","[-1,0)","(-infy,-1)"],
-        textinfo: "label+percent",
-        textposition: "outside",
-        automargin: true
-    }];
-    var layout = {
-        height: 400,
-        width: 500
+//  create data for plotting / plot when all data processed
+var traces_box = [];
+function generateBoxPlotly( x )
+{
+    var trace = {
+        type: 'box',
+        name: (""+traces_box.length),
+        x: x,
+        boxmean: 'sd',
+        boxpoints: false,
     };
-    Plotly.newPlot(plotDiv, data, layout);
+    traces_box.push(trace);
+    if ( traces_box.length == 12 ) { makeBoxPlotly(); }
+}
+function makeBoxPlotly()
+{
+    var plotDiv = document.getElementById("box-plot-area");
+    var layout = {};
+    Plotly.newPlot(plotDiv, traces_box, layout);
 }
 
 /////////////////////////////////////////////////////////////////////
 // --- MAIN CODE HERE --- 
 //  plot 12 strategies for player0
-histogram_data = [];
+
 for (var i=0; i<12; i++) {
-    readCSVHistogramData(0,i); // new dataset
+    // new dataset
+    readCSVHistogramData(0,i);  // histogram
+    readCSVBoxData(0,i);        // box
 }
 
-
-makePieChart();
